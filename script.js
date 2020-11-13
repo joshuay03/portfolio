98ce0764862a9db546f5298a6f321e29438db840
Joshua Young <djry1999@gmail.com>
2020-11-13 15:08:20 +1000

    Transfer source code from initial project

---
+++ script.js
@@ -0,0 +1,40 @@
+var i = 0;
+var count = 0;
+var txt = ["Name? Joshua Young.", "Pursuit? Computer Science.", "Interests? Cloud and AI.", "What's this? My portfolio.", "Have a look around!"]
+var speed = 40;
+var delayInMilliseconds = 1000;
+
+function checkFirstVisit() {
+    if (document.cookie.indexOf('mycookie') == -1) {
+        document.cookie = 'mycookie = 1';
+        typeWriter();
+    } else {
+        while (i < txt.length) {
+            document.getElementById("intro" + i.toString()).innerHTML = txt[i];
+            i++;
+        }
+        document.getElementById("row").style.display = "flex";
+    }
+}
+
+function typeWriter() {
+    if (i < txt[count].length) {
+        if (txt[count].charAt(i) != '?') {
+            document.getElementById("intro" + count.toString()).innerHTML += txt[count].charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        } else {
+            document.getElementById("intro" + count.toString()).innerHTML += txt[count].charAt(i);
+            i++;
+            setTimeout(typeWriter, delayInMilliseconds);
+        }
+    } else if (count !== 4) {
+        count++;
+        i = 0;
+        setTimeout(typeWriter, delayInMilliseconds);
+    } else {
+        setTimeout(function() {
+            document.getElementById("row").style.display = "flex";
+        }, delayInMilliseconds);
+    }
+}
